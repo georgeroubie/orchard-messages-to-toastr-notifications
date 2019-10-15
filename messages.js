0b5471eb6108f4771cf8a34ab05845f3c197c332
George Roubie <george.roubie@gmail.com>
2019-10-15 23:26:52 +0300

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,24 +1,22 @@
 (function () {
-	//toastr options
+	// Toastr options
 	toastr.options = {
-  	'showDuration': '1000',
-  	'hideDuration': '1000',
-  	'timeOut': '8000',
-  	'progressBar': true,
-  	'positionClass': 'toast-bottom-right'
+		'showDuration': '1000',
+		'hideDuration': '1000',
+		'timeOut': '8000',
+		'progressBar': true,
+		'positionClass': 'toast-bottom-right'
 	};
 	
-	//First, we will hide the messages, you can do it with CSS, but in this example, I will use only JavaScript.
-	$('.zone-messages .message').css('display', 'none');
-	
+	// Convert html messages to toastr notifications
 	var messageElement;
 	$('.zone-messages .message').each(function () {
 		messageElement = $(this);
-		if(messageElement.hasClass('message-Information')){
+		if(messageElement.hasClass('message-Information')) {
 			toastr.success(messageElement.html());
-		} else if(messageElement.hasClass('message-Warning')){
+		} else if(messageElement.hasClass('message-Warning')) {
 			toastr.warning(messageElement.html());
-		} else if(messageElement.hasClass('message-Error')){
+		} else if(messageElement.hasClass('message-Error')) {
 			toastr.error(messageElement.html());
 		}
 	});
